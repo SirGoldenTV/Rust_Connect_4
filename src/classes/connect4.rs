d37d61391d91109df73cf4daec1b5f7e6a6a917f
j.garriguet <j.garriguet@c3rb.fr>
2024-01-26 10:56:51 +0100

    feat: check_line & check_column

--- src/classes/connect4.rs
+++ src/classes/connect4.rs
@@ -22,20 +22,42 @@ impl Connect4 {
         }
     }
 
-    pub fn check_line(&self, _line : u8) {
+    pub fn check_win_line(&self, _line : u8) -> bool {
+        return self.grid[usize::from(_line)].windows(4).any(|window| {
+            window.iter().all(|&num| num == window[0] && num != 0)
+        });
+    }
+
+    pub fn check_win_column(&self, _column : u8) -> bool {
+        return self.grid.windows(4).any(|window| {
+            window.iter().all(|row| row[usize::from(_column)] == window[0][usize::from(_column)] && row[usize::from(_column)] != 0 )
+        });
+    }
+
+    pub fn check_win_diagonal(&self) {
         println!("Pas implémenté.");
     }
 
-    pub fn check_column(&self, _column : u8) {
-        println!("Pas implémenté.");
+    pub fn check_column_full(&self, _col : u8) -> bool {
+        self.grid[0][usize::from(_col)] == 0
     }
 
-    pub fn check_diagonal(&self) {
-        println!("Pas implémenté.");
-    }
+    pub fn play(&mut self, _col : u8, _string : &mut String) {
+        if usize::from(_col) > self.grid[0].len() {
+            _string.clear();
+            _string.push_str("Must be in the array.");
+            return;
+        }
 
-    pub fn play(&self, _col : u8) {
-        let _player_active = self.player;
+        // let _player_active = self.player;
+        // let _column_full_message = String::from("Column full!");
+        // let _game_finished_message = String::from("Game has finished!");
+
+        self.attempt_turn(_string);
+        // self.win_message(&_win_message);
+
+        self.show();
+        self.change_player();
     }
 
     pub fn change_player(&mut self) {
@@ -45,5 +67,19 @@ impl Connect4 {
         else {
             self.player -= 1;
         } 
+    }
+
+    fn attempt_turn(&self, _str : &mut String) {
+        _str.clear();
+        _str.push_str("Player ");
+        _str.push_str(&self.player.to_string());
+        _str.push_str(" has a turn");
+    }
+
+    fn win_message(&self, mut _str : &mut String) {
+        _str.clear();
+        _str.push_str("Player ");
+        _str.push_str(&self.player.to_string());
+        _str.push_str(" wins!");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use classes::connect4::Connect4;
 fn main() {
     println!("Hello, World!");
     let mut _game = Connect4::new();
+    let mut _str = String::new(); 
     _game.show();
-    _game.check_line(0);
-    _game.check_column(0);
-    _game.check_diagonal();
+    _game.check_win_line(0);
+    _game.check_win_column(0);
+    _game.check_win_diagonal();
     _game.change_player();
-    _game.play(0);
+    _game.play(0, &mut _str);
 }
